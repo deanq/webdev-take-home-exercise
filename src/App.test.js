361da470e0600f5ea1b40bd251d5529b58df0e21
Dean Quiñanola <deanq@deanq.com>
2018-11-25 18:40:36 -0500

    App basic test

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,15 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  shallow(<App />);
-});
+describe('App initializing', () => {
+  let component;
+
+  beforeEach(() => {
+    component = shallow(<App />);
+  });
+
+  it('renders without crashing', () => {
+    expect(component.exists()).toEqual(true);
+  });
+})
+
