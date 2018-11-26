85cc1614a9474fa7b94488af02330d5fb4d75485
Dean Quiñanola <deanq@deanq.com>
2018-11-25 22:43:25 -0500

    Backend uses npm: nunjucks, helmet and compression
    Preparing to serve /build as view template and static serving

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+var helmet = require('helmet');
+var compression = require('compression');
 
 var app = express();
 
 app.use(logger('dev'));
+app.use(helmet());
+app.use(compression());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+
+// jinja-style template views
+var nunjucks = require('nunjucks');
+nunjucks.configure(['build', 'views'], {
+  autoescape: true,
+  express: app
+});
+
+// serve /build as static, except for *.html files
+app.use('/', (req, res, next) => {
+  var result = req.url.match(/^\/.+\.html$/)
+  if (result) {
+    return res.status(404).send()
+  }
+  next()
+});
+app.use(express.static(path.join(__dirname, 'build')));
 
 module.exports = app;
