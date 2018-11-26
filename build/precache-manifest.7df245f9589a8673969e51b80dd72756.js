bc2e0416de1dd8ef15238868fd0bdf6f12257de9
Dean Quiñanola <deanq@deanq.com>
2018-11-25 23:02:55 -0500

    Allow /build, Ignore /src and /public

---
+++ build/precache-manifest.7df245f9589a8673969e51b80dd72756.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6a24050b4ac04c34b620",
+    "url": "/static/js/main.6a24050b.chunk.js"
+  },
+  {
+    "revision": "bb0b5fde4d8cfe4974b8",
+    "url": "/static/js/1.bb0b5fde.chunk.js"
+  },
+  {
+    "revision": "6a24050b4ac04c34b620",
+    "url": "/static/css/main.8f1cba94.chunk.css"
+  },
+  {
+    "revision": "8402fe49a3bf1deeda67998724daebc4",
+    "url": "/index.html"
+  }
+];
